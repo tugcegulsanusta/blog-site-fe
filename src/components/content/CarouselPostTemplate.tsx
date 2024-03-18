009f059d0d242eeac387f4bca5400e396e98046d
Tugce Usta <tugce.gulsan@gmail.com>
2024-03-18 12:27:43 +0300

    create fake data

--- src/components/content/CarouselPostTemplate.tsx
+++ src/components/content/CarouselPostTemplate.tsx
@@ -5,9 +5,15 @@ import { useNavigate } from "react-router-dom";
 
 export default function CarouselPostTemplate(product) {
   const navigate = useNavigate();
-  function handleClick(){
-    navigate("/post{id}")
+  function handleClick() {
+    navigate(`/post${product.id}`)
   }
+  const [decodedImage, setDecodedImage] = useState<string | null>(null);
+
+  const [photoUrl, setPhotoUrl] = useState<string>("");
+
+
+
   return (
     <div
       style={{
@@ -23,22 +29,24 @@ export default function CarouselPostTemplate(product) {
       >
         <div className="mb-2">
           <img
-            src={`https://primefaces.org/cdn/primereact/images/product/${product.base64img}`}
+            src={product.base64img}
             alt={product.base64img}
             className="w-48 h-48 object-cover shadow-2 mx-auto"
+            style={{ maxWidth: '60%', maxHeight: '60%' }}
           />
         </div>
         <div>
           <h4 className="mb-3 text-lg font-semibold">{product.header}</h4>
-          <h6 className="mt-0 mb-3 text-sm">{product.content}</h6>
+          <h6 className="mt-0 mb-3 text-sm">{product.content.slice(0, 250)}...</h6>
           <div className="mt-5 flex flex-wrap gap-2 justify-content-center">
             <Button
               icon="pi pi-link"
               rounded
               style={{ backgroundColor: "#6c757d", borderColor: "#6c757d" }}
               onClick={handleClick}
-            />
+            /><p className="views">Görüntüleme: {product.view}</p>
           </div>
+
         </div>
       </div>
     </div>
